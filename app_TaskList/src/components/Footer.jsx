2ec0c23c4598db5f09f436b7acdf922b6c466327
Federico Adach <fadach@gmail.com>
2023-11-17 17:33:59 -0300

    footer se ajusto margintop a 15px

--- app_TaskList/src/components/Footer.jsx
+++ app_TaskList/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 export default function Footer() {
   const año = new Date().getFullYear()
     return (
-    <AppBar position="static" color="primary" sx={{marginTop:'auto'}}> 
+    <AppBar position="static" color="primary" sx={{marginTop:'15px'}}> 
       <Toolbar>
         <Typography variant="body2" color="inherit">
            Administrador de Tareas - TP UTN - REACT  © {año} .
